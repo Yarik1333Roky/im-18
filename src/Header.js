7f650e7d0b0c614b0f607dd7e808783aa1b4265c
exeyarikus <mr.yarik1333@gmail.com>
2025-09-25 22:33:55 +0300

    what

--- src/Header.js
+++ src/Header.js
@@ -2,20 +2,20 @@ import './Header.css';
 
 function Header() {
   return (
-    <header className="header">
+    <header className="header" role="banner">
       <div className="first-header">
-        <div className="logo"><p>18</p></div>
-        <div className="main-header">
-          <a className="header-zagolovok">Главная</a>
-          <div className="header-border" />
-          <a className="header-zagolovok">Расписание</a>
-          <div className="header-border" />
-          <a className="header-zagolovok">Домашнее задание</a>
-        </div>
+        <div className="logo" aria-label="Логотип группы"><p>18</p></div>
+        <nav className="main-header" role="navigation" aria-label="Основное меню">
+          <a className="header-zagolovok" role="button">Главная</a>
+          <div className="header-border" aria-hidden="true" />
+          <a className="header-zagolovok" role="button">Расписание</a>
+          <div className="header-border" aria-hidden="true" />
+          <a className="header-zagolovok" role="button">Домашнее задание</a>
+        </nav>
       </div>
-      <a className="second-header">
-        <img className="params" src="/params.svg" />
-      </a>
+      <button className="second-header" aria-label="Настройки">
+        <img className="params" src="/params.svg" alt="Иконка настроек" />
+      </button>
     </header>
   );
 }
